7c4144f7719fa5b907190b36652234988799f6b8
Lathiya Jayesh <lathiyajayesh77@gmail.com>
2024-09-05 20:59:11 +0530

    feat : added new extention method on BuildContext

--- lib/src/utility/pb_extension.dart
+++ lib/src/utility/pb_extension.dart
@@ -16,6 +16,20 @@ extension OnRow on Row {
 
 // ------- Logic -------
 
+extension NavigatorContext on BuildContext {
+  void pop<T extends Object?>([T? result]){
+    return Navigator.of(this).pop();
+  }
+
+  Future<T?> pushNamed<T extends Object?>(
+    String routeName,{
+    Object? arguments,
+  }){
+    return Navigator.of(this).pushNamed(routeName, arguments:arguments);
+  }
+
+}
+
 extension OrDefault<T> on T? {
   T get orDefault {
     final value = this;
