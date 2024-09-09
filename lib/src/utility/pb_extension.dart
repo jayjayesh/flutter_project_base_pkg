f4e630bbee1f6ecf475faa4160c0da4f20f4b832
Jayesh Lathiya <jayjayesh@users.noreply.github.com>
2024-09-09 18:22:08 +0530

    feat : Update pb_extension.dart

--- lib/src/utility/pb_extension.dart
+++ lib/src/utility/pb_extension.dart
@@ -49,6 +49,25 @@ extension OnTextButton on TextButton{
 // ------- Logic -------
 
 extension OnBuildContext on BuildContext {
+/// Screen Size
+  bool get isMobile => MediaQuery.of(this).size.width <= 500.0;
+
+  bool get isTablet => MediaQuery.of(this).size.width < 1024.0 && MediaQuery.of(this).size.width >= 650.0;
+
+  bool get isSmallTablet => MediaQuery.of(this).size.width < 650.0 && MediaQuery.of(this).size.width > 500.0;
+
+  bool get isDesktop => MediaQuery.of(this).size.width >= 1024.0;
+
+  bool get isSmall => MediaQuery.of(this).size.width < 850.0 && MediaQuery.of(this).size.width >= 560.0;
+
+  double get width => MediaQuery.of(this).size.width;
+
+  double get height => MediaQuery.of(this).size.height;
+
+  Size get size => MediaQuery.of(this).size;
+  
+
+/// Navigation
   void pop<T extends Object?>([T? result]){
     return Navigator.of(this).pop();
   }
