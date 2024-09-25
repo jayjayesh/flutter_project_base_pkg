d2406ba13198aa1de3d9a55c85f7b712166a8fdd
lathiyajayesh77 <lathiyajayesh77@gmail.com>
2024-09-25 07:31:29 +0000

    feat : added new method to preload svg assets

--- lib/src/utility/pb_ui_utility.dart
+++ lib/src/utility/pb_ui_utility.dart
@@ -1,3 +1,4 @@
+import "package:flutter_svg/flutter_svg.dart";  
 part of flutter_project_base;
 
 class PBUIUtility {
@@ -19,4 +20,34 @@ class PBUIUtility {
         ),
       );
   }
+
+  /// ------- SVG -------
+  ///
+  /// ------- How to use it? ---------- 
+  /// use this preloadSVGFiles method in main.dart before run app
+  ///
+  /// void main() async {
+  ///   WidgetsFlutterBinding.ensureInitialized();
+  ///   await preloadSVGFiles([
+  ///     'assets/my_icon1.svg',
+  ///     'assets/my_icon2.svg',
+  ///     'assets/my_icon3.svg',
+  ///   ]);
+  ///   runApp(MyApp());  
+  /// }
+  /// ----------------------------------
+  ///
+  Future<void> preloadSVGFiles(List<String> svgPaths) async{
+    for(final svg in svgPaths){
+      final svgLoader = SvgAssetsLoader(svg);
+      await svg.catch.putIfAbsent(
+        svgLoader.cacheKey(null),
+        () => svgLoader.loadBytes(null),
+      );
+    }
+    return Future.wait(svgPaths.map((path) async {
+      await precachePicture(ExactAssetPicture(AssetName(path)), null);
+    }));
+  }
+
 }
